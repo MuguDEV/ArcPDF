0dd02de00013758bbabdb514cad78fa20b4233e5
Mugunthan <the.mugunthan@gmail.com>
2026-05-14 00:25:31 +0530

    Set Kotlin JVM target to version 17
    Configure Kotlin compiler options for JVM target version 17 in subprojects.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,7 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
     
     afterEvaluate {
         if (project.hasProperty("android")) {
@@ -23,6 +24,13 @@ subprojects {
                     targetCompatibility = JavaVersion.VERSION_17
                 }
             }
+        }
+    }
+}
+subprojects {
+    project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 }
